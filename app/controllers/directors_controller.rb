5e90b1379ae6be49dfcd880e6ba349fd6cd897ac
gkrishn0 <gkrishn0@chicagobooth.edu>
2021-04-28 19:42:56 +0000

    eldest/youngest both work

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,6 +1,23 @@
 class DirectorsController < ApplicationController
   def index
     @list_of_directors=Director.all
+    
     render({ :template=> "director_template/index.html.erb"})
+  end
+  def wisest
+    @list_of_directors=Director.all
+    @oldest=Director.where.not({ :dob=> nil}).order({ :dob => :asc }).at(0).name
+    @dob = Director.where.not({ :dob=> nil}).order({ :dob => :asc }).at(0).dob
+    render({ :template=> "director_template/eldest.html.erb"})
+  end
+
+  def youth
+    @list_of_directors=Director.all
+    @youngest=Director.where.not({ :dob=> nil}).order({ :dob => :desc }).at(0).name
+    @dob = Director.where.not({ :dob=> nil}).order({ :dob => :desc }).at(0).dob
+    #@oldest=Director.where.not({ :dob=> nil}).order({ :dob => :asc }).at(0).name
+    #@dob = Director.where.not({ :dob=> nil}).order({ :dob => :asc }).at(0).dob
+    @youngest= Director.order({ :dob => :desc }).at(0)
+    render({ :template=> "director_template/youth2.html.erb"})
   end
 end
