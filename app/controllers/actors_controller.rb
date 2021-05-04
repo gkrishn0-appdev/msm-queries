635cd024eaf2f711e28694aef5e676cd67887e0f
gkrishn0 <gkrishn0@chicagobooth.edu>
2021-05-04 16:45:50 +0000

    100%

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -9,8 +9,8 @@ class ActorsController < ApplicationController
   def movie_details
     params.fetch("an_id")
     @the_id = params.fetch("an_id")
-    @the_director = Director.where({ :id => @the_id }).at(0)
-    @filmography = Movie.where({:director_id => @the_director.id})
+    @the_director = Actor.where({ :id => @the_id }).at(0)
+    @filmography = Character.where({:actor_id => @the_director.id})
     render({ :template=> "actor_template/show.html.erb"})
   end
 
